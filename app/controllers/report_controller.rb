47a9b4da4628a7592e193f7c8648ade8d7d3d510
Dastan75 <raisos@hotmail.fr>
2014-09-24 10:59:19 +0200

    41.5

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,79 +1,26 @@
-def parseHx()
-	@err[:hx] = []
-	if @site.nil?
-		return nil
-	end
-	@site.pages.each do |p|
-		if p.hxes.first.nil?
-			next
-		end
-		orderBool = false;
-		prv = p.hxes.first.x
-		max = prv
-		p.hxes.each do |h|
-			if (h.x - prv).abs > 1 
-				@err[:hx].append({
-					:curr => h,
-					:file => p,
-					:type => ERR_HX_DIFF
-					})
-			end
-			if (h.x < max) and !orderBool
-				@err[:hx].append({
-					:curr => h,
-					:file => p,
-					:type => ERR_HX_ORDER
-					})
-				orderBool = true
-			end
-			prv = h.x
-		end
-	end
-end
 
 class ReportController < ApplicationController
 	before_filter :load
 
 	def bypage
-		@err = {}
-		@err[:hx] = []
 		p = Site.find(params[:id]).pages.all.where(id:params[:pid]).first
 		@Page = Site.find(params[:id]).pages.all.where(id:params[:pid]).all
 		@Site = Site.find(params[:id])
 		if p.hxes.first.nil?
 			return
 		end
-		@err[:hx].append("Pas d'erreurs de balises")
-#		orderBool = false;
-		prv = p.hxes.first.x
-		p.hxes.each do |h|
-			if (h.x - prv).abs > 1 
-				@err[:hx].append({
-					:curr => h,
-					:file => p,
-					:type => ERR_HX_DIFF
-					})
-			end
-		end
 	end
 	def load
-
 	end
 	def show
-		@err = {}
 		if (@site = Site.find(params[:id])) == nil
 			redirect_to root_path
 		end
-		parseHx()
 	end
 	def hx		
-		@err = {}
 		if (@site = Site.find(params[:id])) == nil
 			redirect_to root_path
 		end
-		parseHx()
-		@hxFiles = []
-		@err[:hx].each {|h| @hxFiles << h[:file].id}
 	end
 	def img
 	end
@@ -82,9 +29,7 @@ class ReportController < ApplicationController
 		# Owi !!!
 	end
 	def index
-		@err = {}
 		@sites = Site.all
-		parseHx()
 	end
 	def destroy
 		Site.find(params[:id]).destroy
