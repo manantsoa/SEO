e2d30b8ec4ed015e2fa086f2be8ebdb04e2ff640
Xide <germain.gau@epitech.eu>
2014-09-26 15:26:44 +0200

    SEOSoft for the noobs

---
+++ rank.rb
@@ -0,0 +1,76 @@
+# This > all > SEO Soft :)
+
+require 'open-uri'
+require 'nokogiri'
+require 'rubygems'
+require (`uname` == "Linux\n" ? 'mysql' : 'pg')
+require 'yaml'
+require 'active_record'
+
+y = YAML.load_file('./config/database.yml')["development"]
+ActiveRecord::Base.establish_connection(y)
+
+class Site < ActiveRecord::Base
+	has_many :positions, :dependent => :destroy
+end
+
+class Position < ActiveRecord::Base
+	belongs_to :site
+end
+
+def posInPage(host, raw)
+	pos = 0
+	begin
+		fmt = Nokogiri(raw)
+	rescue Exception => e
+		puts e.to_s
+		return pos
+	end
+	orderFmt = fmt.css("h3").sort_by {|t| t.line}
+	orderFmt.each do |bal|
+		pos += 1 unless bal.to_s.include? "google"
+		#puts "=========================== #{pos}"
+		#puts bal
+		return pos if bal.to_s.include? host
+	end
+	puts "Root not found in h3. (Should never happen)"
+	return (0)
+end
+
+def googlePos(url, keywords, depth = 10, verbose = true)
+	url = 'http://' + url.to_s unless url.to_s.start_with?('http://') || url.to_s.start_with?('https://')
+	url = URI(url) unless url.kind_of? URI::HTTP 
+	keywords = [keywords] unless keywords.is_a? Array
+	ret = Hash.new
+
+	puts url.to_s
+	return if url.host.nil?
+	puts "Recherche de #{url}" if verbose
+	keywords.each do |keyword|
+		puts "\t=> #{keyword}" if verbose
+		keyword.gsub! ' ', '+'
+		ret[keyword] = 0
+		depth.times do |pageNum|
+			puts "\t   Page #{pageNum + 1}" if verbose
+			query = "https://www.google.fr/search?q=#{keyword}&start=#{10 * pageNum}"
+			begin
+				resp = open(query).read
+			rescue Exception => e
+				puts e.to_s
+				puts e.traceback if verbose
+				resp = "<html></html>"
+			end
+			if resp.include? url.host
+				ret[keyword] = 10 * pageNum + posInPage(url.host, resp) 
+				break
+			end		
+		end
+	end
+	return ret
+end
+
+keyw = []
+(1..ARGV.size - 1).each {|s| keyw.append(ARGV[s].dup)}
+#puts keyw
+#puts ARGV[0]
+puts googlePos(ARGV[0], keyw).each {|key, value| Position.create(query:key, pos:value)}
