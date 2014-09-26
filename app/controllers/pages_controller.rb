fe2d0bed80b6b43773f2a22330fbdbfbc1b8ec72
Dastan75 <raisos@hotmail.fr>
2014-09-26 09:42:31 +0200

    name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
   end
   def submitCrawl
     url = URI.parse(params[:site][:url])
-  	Site.create(name:params[:site][:name], url:url.to_s) unless Site.where(url:url.to_s).count > 0
+  	Site.create(url:url.to_s) unless Site.where(url:url.to_s).count > 0
     spawn("ruby crawler.rb \"" + url.to_s + "\"")
   	redirect_to root_path
   end
